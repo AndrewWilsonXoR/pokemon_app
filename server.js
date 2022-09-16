7535ef2dd5cba60ce6dbbe91d295d15a8a1dfc7a
AndrewWilsonXoR <110188732+AndrewWilsonXoR@users.noreply.github.com>
2022-09-16 12:14:24 -0400

    Add files via upload
    "Show view shows req.params.id"

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine());
-const pokemon = require('./models/pokemon.js')
+const pokemon = require('./models/pokemon')
 
 
 app.get('/', (req, res) => {
@@ -10,6 +10,9 @@ app.get('/', (req, res) => {
 });
 app.get('/pokemon', (req, res) =>{
     res.render('Index.jsx', {pokemon})
+})
+app.get('/pokemon/:id', (req, res) =>{
+    res.send(req.params.id);
 })
 app.listen(3000, () =>{
     console.log('listening')
